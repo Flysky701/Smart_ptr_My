b0ade21aa4b43b88e7b3a932b0e07cfc5f396d8a
Flysky701 <18458871liu@gmail.com>
2025-02-12 01:04:18 +0800

    完成shared_ptr(未debug)

--- include/Smartptr.h
+++ include/Smartptr.h
@@ -2,16 +2,71 @@
 #include <atomic> 
 
 template <typename T>
-class my_smart_ptr{
+class shared_ptr{
     public:
-        shared_ptr(T* ptr_ = nullptr);
-        explicit shared_ptr()
-    
+        explicit shared_ptr(T* ptr_ = nullptr) noexcept
+            : ptr_(ptr), count_(ptr ? new std::atomic<size_t> (1) : nullptr)
+        {}
+        
+        shared_ptr(const shared_ptr& other) noexcept
+            : ptr_(other.ptr_), count_(other.count_)
+        {
+            if(count_){
+                count -> fetch_add(1, std::memor_order_relaxed);
+            }
+        }
+
+        shared_ptr(shared_ptr&& other) noexcept
+            : ptr_(other.ptr_), count_(other.count_)
+        {
+            other.ptr_ = nullptr;
+            other.count_ = nullptr;
+        }
+        
+        ~shared_ptr(){
+            release();
+        }
+
+        shared_ptr& operator =(const shared_ptr& other) noexcept{
+            if(this != &other){
+                release();
+                ptr_ = other.ptr_;
+                count_ = other.count_;
+                if(count_){
+                    count_ -> fetch_add(1);
+                }
+            }
+            return *this;
+        }
+        
+        shared_ptr& operator =(const shared_ptr&& other) noexcept{
+            if(this != &other){
+                release();
+                ptr_ = other.ptr_;
+                count_ = other.count_;
+                other.count_ = nullptr;
+                other.ptr_ = nullptr;
+            }
+        }
+        
+        T& operator* ()const noexcept{
+            return *ptr_;
+        }
+
+        T& operator-> ()const noexcept{
+            return ptr_;
+        }
+
+        size_t usecount() const noexcept{
+            return count_ ? count_-> load(std::memory_order_relaxed): 0;
+        }
+        
+
     private:
         T* ptr_;
         std::atomic<size_t>* count_;
 
-        void relese() noexcept{
+        void release() noexcept{
             if(count_){
                 if(count_ -> fetch_sub(1) == 1){
                     delete ptr_;
@@ -21,8 +76,4 @@ class my_smart_ptr{
                 count_ = nullptr;
             }
         }
-
-
-
-
 };
